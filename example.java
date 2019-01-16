e9ee41b5b2588431fc32bff2e32517cccedfec05
Jamie Huddlestone <jhuddlestone1@sheffield.ac.uk>
2019-01-16 11:00:03 +0000

    Replace example.java

--- example.java
+++ example.java
@@ -1,26 +1,65 @@
 
-boolean dirIsLeft = false;
+public class Example {
+	
+	private static void goForward() {
+		leftMotor.forward();
+		rightMotor.forward();	
+    }
+	
+	private static void goBackward(int count) {
+    	leftMotor.backward();
+    	rightMotor.backward();
+    }
+	
+    private static void turnLeft(int count) {
+    	leftMotor.backward();
+    	rightMotor.forward();
+    }
+		
+    private static void turnRight(int count) {
+    	leftMotor.forward();
+    	rightMotor.backward();
+    }
+	
+	private static void stop() {
+		leftMotor.stop();
+		rightMotor.stop();
+	}
+	
+	public static void main(String[] args) {
+	
+		// Robot setup
+		Robot robot = new Robot("dia-lego-e2");
+		Speaker speaker = robot.getSpeaker();
+		Motor leftMotor = robot.getLargeMotor(Motor.Port.B);
+		Motor rightMotor = robot.getLargeMotor(Motor.Port.C);
+		
+		boolean dirIsLeft = false;
 
-while (true) {
+		while (true) {
 
-	switch (sensor.getColor()) {
+			switch (sensor.getColor()) {
+				/*
+				case YELLOW:
+					yellowDot();
+					break;
+					
+				case RED:
+					redDot();
+					break;
+				*/
+				case WHITE:
+					dirIsLeft ? turnLeft(10) : turnRight(10);
+					dirIsLeft = !dirIsLeft;
+				
+				case BLACK:
+				default:
+					goForward(10);
+				
+			}
+		}
 		
-		case YELLOW:
-			yellowDot();
-			break;
-			
-		case RED:
-			redDot();
-			break;
-		
-		case WHITE:
-			dirIsLeft ? turnLeft() : turnRight();
-			dirIsLeft = !dirIsLeft;
-		
-		case BLACK:
-		default:
-			goForward();
+		robot.close();
 		
 	}
-
 }
