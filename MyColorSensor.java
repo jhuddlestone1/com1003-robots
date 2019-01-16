5a32ac4e861666b43a193eb1b8a721adc7ea6a4c
Karolina Debowska <kbdebowska1@sheffield.ac.uk>
2019-01-16 09:50:50 +0000

    Update MyColorSensor.java

--- MyColorSensor.java
+++ MyColorSensor.java
@@ -28,7 +28,7 @@ public class MyColorSensor {
     	sensor = robot.getColorSensor(Sensor.Port.S1);
     	
     	// the robot goes forward till black color is detected
-    	scanBlack(ColorSensor.Color.BLACK); // a wrong value 20
+    	//scanBlack(ColorSensor.Color.BLACK); // a wrong value 20
 		
 		// go forward till the end of the black path
 		scanOffThePath(ColorSensor.Color.BLACK); // a wrong value 20 
@@ -49,6 +49,21 @@ public class MyColorSensor {
     	robot.close();
     }
 	
+	private static void yellowDot(){
+			while (col != ColorSensor.Color.YELLOW){
+				//WORK ON MORE ELABORATE METHOD, HOW TO MEASURE THE DISTANCE TO CENTRE
+				//OF CIRCLE? MANUAL MEASURE OR ROBOT SHOULD FIGURE OUT THE CENTRE	
+				scanOffThePath(ColorSensor.Color.BLACK);
+				col = sensor.getColor();
+				if (col == color){
+					//release the ball 
+					for (int i=0;i<3;i++){
+						turnLeft(2);
+						turnRight(2);
+					}
+				}
+	}
+
 		private static void blackDot(){
 			while (col != ColorSensor.Color.BLACK){
 				//WORK ON MORE ELABORATE METHOD, HOW TO MEASURE THE DISTANCE TO CENTRE
@@ -60,27 +75,38 @@ public class MyColorSensor {
 			}
 		}
 		
-		 private static void scanRed(boolean forward, ColorSensor.Color color) {
+		 private static void scanRed(ColorSensor.Color color) {
     	//System.out.println("Scanning " + (right ? "right" : "left") + " for " + count);
 			while (col !=color){
-				goForward(500); //a wrong value
+				scanOffThePath(); //a wrong value
 				col = sensor.getColor();			
 			}
-	    //make sound
+			if (col == color){
+				//It can recognise the red dot by singing
+				//And attempt to pick up its ping pong ball
+				//turn 180 degress
+				//go back to the yellow dot: 
+				//scanOffThePath(ColorSensor.Color.BLACK);
+				//yellowDot();
+				//release the ball;
+	    
     }
     
     // This method moves the robot forward by count "steps"
     // Technically, the steps are degrees of rotation of the motors
     // but how this translates to actual distances moved will depend
     // on various factors...
-    private static void goForward(int duration) {
+    private static void goForward() {
+		leftMotor.forward();
+		rightMotor.forward();	
+    }
+	/*private static void goForward() {
 		leftMotor.forward();
 		rightMotor.forward();
 		robot.sleep(duration);
 		leftMotor.stop();
 		rightMotor.stop();		
-    }
-	
+    }*/
     // This turns the robot to the right by movinf the left
     // motor forward and the right motor backwards.
     // The count value is the number of degrees of rotation 
@@ -126,7 +152,7 @@ public class MyColorSensor {
     private static void scanBlack(ColorSensor.Color color) {
     	//System.out.println("Scanning " + (right ? "right" : "left") + " for " + count);
     	while (col != color) {
-			goForward(100); //a wrong value
+			goForward(); //a wrong value
     		col = sensor.getColor();			
     		if(col == color) {
     			turnLeft(90);
@@ -136,11 +162,11 @@ public class MyColorSensor {
 	private static void scanOffThePath(ColorSensor.Color color) {
     	//System.out.println("Scanning " + (right ? "right" : "left") + " for " + count);
 		//change for loops to while loops ! (delete int count)
-    	while (true){
+    	/*while (true){
     		col = sensor.getColor();			
     		if(col == color) {
 				turnRight(2);
-				goForward(200);
+				goForward();
 				col = sensor.getColor();
 			}
 			else if (col == ColorSensor.Color.RED){
@@ -148,8 +174,32 @@ public class MyColorSensor {
 			}
 			else {
 				turnLeft(4);
-				goForward(200);
+				goForward();
 				col = sensor.getColor();
+			} 
+    	}*/
+			while (true){
+    		col = sensor.getColor();			
+    		if(col == color) {
+				goForward();
+				col = sensor.getColor();
+			}
+			else if (col == ColorSensor.Color.RED){
+				break;
+			}
+			else {
+				while (col != color){
+				int i=1;
+				turnRight(i);
+				col = sensor.getColor();
+				if (col == color) 
+					scanOffThePath(ColorSensor.Color.BLACK);
+				else{					
+				    turnLeft(2*i);
+				    col = sensor.getColor();
+				}
+				i++;
+				}
 			} 
     	}
 		/*another version
